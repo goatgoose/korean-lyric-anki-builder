9e09198c411e90e192b187ca21985c0aa38f2c0a
Sam Clark <sam.clark1142@gmail.com>
2019-09-08 22:49:17 -0400

    builds anki deck

--- AnkiBuilder.py
+++ AnkiBuilder.py
@@ -1,10 +1,31 @@
 from lyricsgenius import Genius
 from gtts import gTTS
+from googletrans import Translator
+import genanki
 
 
 class AnkiBuilder:
-    def __init__(self):
+    def __init__(self, deck_name, deck_id):
+        self.deck_name = deck_name
+        self.deck_id = deck_id
+
         self.genius = Genius("DhLgBRFiLg4HOiABUzSJvGjpTDgX6vwVSFvyV3P6igiKdAhNdvq0VbwnF8QEXxa0")
+        self.translator = Translator()
+
+        self.model = genanki.Model(
+            1313562696, "lyrics-model",
+            fields=[
+                {"name": "Term"},
+                {"name": "Translation"}
+            ],
+            templates=[
+                {
+                    "name": "term card",
+                    "qfmt": "{{Term}}",
+                    "afmt": "{{FrontSide}}<hr id='answer'>{{Translation}}"
+                }
+            ]
+        )
 
     @staticmethod
     def char_is_hangul(char):
@@ -20,25 +41,45 @@ class AnkiBuilder:
                 return False
         return True
 
-    def get_hangul_lyrics(self, song_name, artist=""):
-        song = self.genius.search_song(song_name, artist=artist)
+    @staticmethod
+    def get_hangul_blocks(song):
+        lines = []
+        for line in song.lyrics.split("\n"):
+            blocks_in_line = []
 
-        words = []
-        for line in song.lyrics.split():
             line = line.strip()
             if line.startswith("["):
                 continue
 
             for word in line.split(" "):
-                if AnkiBuilder.block_is_hangul(word):
-                    words.append(word)
+                if AnkiBuilder.block_is_hangul(word) and word:
+                    blocks_in_line.append(word)
+            if len(blocks_in_line) > 0:
+                lines.append(blocks_in_line)
 
-        return words
+        return lines
 
     def build(self):
-        pass
+        hangul = self.get_hangul_blocks(self.genius.search_song("likey", artist="TWICE"))
+
+        translated = {}
+        for line in hangul:
+            phrase = " ".join(line)
+            translated[phrase] = self.translator.translate(phrase).text
+
+            for block in line:
+                translated[block] = self.translator.translate(block).text
+
+        deck = genanki.Deck(self.deck_id, self.deck_name)
+        for term in translated:
+            deck.add_note(genanki.Note(
+                model=self.model,
+                fields=[term, translated[term]]
+            ))
+
+        genanki.Package(deck).write_to_file(self.deck_name + ".apkg")
 
 
 if __name__ == '__main__':
-    builder = AnkiBuilder()
-    print(builder.get_hangul_lyrics("hold me tight", artist="TWICE"))
+    builder = AnkiBuilder("Likey", 1768730765)
+    builder.build()
